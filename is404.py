31c6a8b5a2a12cc510351e59f10914557a1ef811
co-devs <michael.devens2@gmail>
2017-12-07 15:19:11 -0700

    Added new functions to handle responses and output them, reducing code repetition. Added output to file option. use -o filename where filename is a nonexistant file. Will raise an error if you try to output to a preexisting file. Added placeholder for MySQL command output.
    Signed-off-by: co-devs <michael.devens2@gmail>

--- is404.py
+++ is404.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup as bs
 import urllib
 import argparse
+import os.path
 
 
 def badURL(url):
@@ -67,9 +68,7 @@ def idURL(url, r):
     if "facebook" not in str(url):
         # If not a facebook page, and page opened,
         # We are currently ASSUMING that the page is good
-        response = goodURL(url)
-        if response:
-            print(response)
+        outputResponse(handleResponse(url, 'good'))
     else:
         # If facebook, test it
         testFacebookURL(url, r)
@@ -84,20 +83,14 @@ def testFacebookURL(url, r):
     # If the div that would contain login error text exists
     if ("You must log in to continue" in str(haltDiv)):
         # Then pring questionable URL.  We can't check it w/o logging in
-        response = questionURL(url)
-        if response:
-            print(response)
+        outputResponse(handleResponse(url, 'question'))
     # Else, if we don't need to log in but get a facebook 404
     elif ("not found" in str(fourDiv)):
         # Then the page does not exist
-        response = badURL(url)
-        if response:
-            print(response)
+        outputResponse(handleResponse(url, 'bad'))
     else:
         # Otherwise, the facebook page probably exists
-        response = goodURL(url)
-        if response:
-            print(response)
+        outputResponse(handleResponse(url, 'good'))
 
 
 def handleURLs(urls):
@@ -106,15 +99,48 @@ def handleURLs(urls):
         # Try to open the url
         try:
             r = urllib.urlopen(fixURL(url))
-        # If the URL cannot be opened, call badURL()
+        # If the URL cannot be opened, handle bad response
         except Exception:
-            response = badURL(url)
-            if response:
-                print(response)
+            outputResponse(handleResponse(url, 'bad'))
             # Move to next URL in list or quit if at end
             continue
         # If URL is good, identify it with idURL()
         idURL(url, r)
+
+
+def handleResponse(url, status):
+    # If bad URL and not suppressed, then call badURL() and return response
+    if status.lower() == 'bad':
+        response = badURL(url)
+        if response:
+            return response
+    # # If good URL and not suppressed, then call goodURL() and return response
+    elif status.lower() == 'good':
+        response = goodURL(url)
+        if response:
+            return response
+    # If question URL and not suppressed, then call questionURL() and
+    # return response
+    elif status.lower() == 'question':
+        response = questionURL(url)
+        if response:
+            return response
+    else:
+        return 'Error in handleResponse'
+
+
+def outputResponse(response):
+    # If output to file flag is set
+    if args.output:
+        # Open file to append response
+        f = open(args.output, 'a+')
+        # Append response
+        f.write(response + '\n')
+    else:
+        # print response to terminal.  Probably some unnecessary repetition of
+        # if response: here since it's also in handleResponse()
+        if response:
+            print(response)
 
 
 if __name__ == "__main__":
@@ -137,6 +163,8 @@ if __name__ == "__main__":
                         help='Do not print questionable URLs')
     parser.add_argument('-o', '--output', metavar='File',
                         help='Print results to file')
+    parser.add_argument('-m', '--mysql', metavar='mySQL',
+                        help='output in MySQL command format')
     parser.add_argument('-t', '--test', action='store_true',
                         help="""test flag, does not return friendly output.  1 =
                         URL exists, 0 = does not exist, -1 = manual check.
@@ -144,6 +172,10 @@ if __name__ == "__main__":
                         to output based on code.""")
     args = parser.parse_args()
     # If provided a list of URLs in command line
+    if args.output:
+        if os.path.isfile(args.output):
+            print('Error, file exists. Please enter a new file name')
+            raise SystemExit
     if args.urls:
         # Call handleURLs() on list
         handleURLs(args.urls)
