e06272306ff6e6ab835013ce8c3752604fb5307d
co-devs <michael.devens2@gmail>
2017-12-07 10:24:14 -0700

    Added comments throughout code.  Implemented suppression of the 3 possible responses.  Can suppress any combination of the three. Added flag to output to file instead of screen, not yet implemented. Reduced code repetition by removing URL handling from the main body of the script and instead defining it in a function.  The main body just calls that function now.
    Signed-off-by: co-devs <michael.devens2@gmail>

--- is404.py
+++ is404.py
@@ -5,60 +5,116 @@ import argparse
 
 
 def badURL(url):
-    if args.test:
-        return '0'
+    # If we are not suppressing bad url output
+    if not args.suppress_bad_urls:
+        # If test flag present
+        if args.test:
+            # Return code 0
+            return '0'
+        else:
+            # Else return proper string
+            return str(url).strip('\n') + ' does not exist'
+    # If we are suppressing
     else:
-        return str(url).strip('\n') + ' does not exist'
+        # Return None
+        return None
 
 
 def goodURL(url):
-    if args.test:
-        return '1'
+    # If we are not suppressing good url output
+    if not args.suppress_good_urls:
+        # If test flag present
+        if args.test:
+            # Return code 1
+            return '1'
+        else:
+            # Else return proper string
+            return str(url).strip('\n') + ' exists'
+    # If we are suppressing
     else:
-        return str(url).strip('\n') + ' exists'
+        # Return None
+        return None
 
 
 def questionURL(url):
-    if args.test:
-        return '-1'
+    # If we are not suppressing questionable url output
+    if not args.suppress_question_urls:
+        # If test flag present
+        if args.test:
+            # Return code -1
+            return '-1'
+        else:
+            # Else return proper string
+            return str(url).strip('\n') + ' is questionable.  Manually check.'
+    # If we are suppressing
     else:
-        return str(url).strip('\n') + ' is questionable.  Manually check.'
+        # Return None
+        return None
 
 
 def fixURL(url):
+    # Check to see if URL contains http
     if "http" not in str(url):
+        # If it is not, then prepend it and return
         return "http://" + url
     else:
+        # If it is then return plain URL
         return url
 
 
 def idURL(url, r):
+    # Identify the kind of site
     if "facebook" not in str(url):
-        if args.test:
-            print(goodURL(url))
-        else:
-            print(goodURL(url))
+        # If not a facebook page, and page opened,
+        # We are currently ASSUMING that the page is good
+        response = goodURL(url)
+        if response:
+            print(response)
     else:
+        # If facebook, test it
         testFacebookURL(url, r)
 
 
 def testFacebookURL(url, r):
     soup = bs(r, "lxml")
-    # Need to be logged in (probably a group)
+    # Get div containing text that indicates page behind a login screen
     haltDiv = soup.find_all("div", class_="_585r")
-    # print haltDiv
+    # Get div containing page does not exist
     fourDiv = soup.find_all("div", class_="fb_content clearfix ")
-    # print soup.prettify().encode('utf-8')
+    # If the div that would contain login error text exists
     if ("You must log in to continue" in str(haltDiv)):
-        print(questionURL(url))
-    # Don't need to be logged in (probably not group)
-    # looking for div class="fb_content clearfix " id="content"
+        # Then pring questionable URL.  We can't check it w/o logging in
+        response = questionURL(url)
+        if response:
+            print(response)
+    # Else, if we don't need to log in but get a facebook 404
     elif ("not found" in str(fourDiv)):
-        # print("Page does not exist")
-        print(badURL(url))
+        # Then the page does not exist
+        response = badURL(url)
+        if response:
+            print(response)
     else:
-        # print("Page exists")
-        print(goodURL(url))
+        # Otherwise, the facebook page probably exists
+        response = goodURL(url)
+        if response:
+            print(response)
+
+
+def handleURLs(urls):
+    # For each URL in list urls
+    for url in urls:
+        # Try to open the url
+        try:
+            r = urllib.urlopen(fixURL(url))
+        # If the URL cannot be opened, call badURL()
+        except Exception:
+            response = badURL(url)
+            if response:
+                print(response)
+            # Move to next URL in list or quit if at end
+            continue
+        # If URL is good, identify it with idURL()
+        idURL(url, r)
 
 
 if __name__ == "__main__":
@@ -70,35 +126,39 @@ if __name__ == "__main__":
                         help='URL(s) to check')
     parser.add_argument('-f', '--file', metavar='File',
                         help='File of URLs, one per line')
+    parser.add_argument('-G', '--suppress-good-urls', action='store_true',
+                        default=False,
+                        help='Do not print good URLs')
+    parser.add_argument('-B', '--suppress-bad-urls', action='store_true',
+                        default=False,
+                        help='Do not print bad URLs')
+    parser.add_argument('-Q', '--suppress-question-urls', action='store_true',
+                        default=False,
+                        help='Do not print questionable URLs')
+    parser.add_argument('-o', '--output', metavar='File',
+                        help='Print results to file')
     parser.add_argument('-t', '--test', action='store_true',
                         help="""test flag, does not return friendly output.  1 =
                         URL exists, 0 = does not exist, -1 = manual check.
                         Intend to implement output to file option, with choice
                         to output based on code.""")
     args = parser.parse_args()
+    # If provided a list of URLs in command line
     if args.urls:
-        for url in args.urls:
-            try:
-                r = urllib.urlopen(fixURL(url))
-            except Exception:
-                # print(url + ' is bad URL, could not open')
-                print(badURL(url))
-                continue
-            idURL(url, r)
+        # Call handleURLs() on list
+        handleURLs(args.urls)
+    # Else if provided a file with URLs
     elif args.file:
         try:
+            # Try to open the file
             f = open(args.file, "r")
         except Exception:
+            # If it fails, return error and exit
             print("Invalid file")
             raise SystemExit
+        # If the file can be opened, read its lines
         urls = f.readlines()
+        # Close file
         f.close()
-        for url in urls:
-            # print str(url).strip('\n')
-            try:
-                r = urllib.urlopen(fixURL(url))
-            except Exception:
-                # print(url + ' is bad URL, could not open')
-                print(badURL(url))
-                continue
-            idURL(url, r)
+        # Call handleURLs() on list
+        handleURLs(urls)
